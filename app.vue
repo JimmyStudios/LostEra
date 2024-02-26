5ebafe7125b93a4e06d3b102db6431d930cd123b
Jalpara <raoaditya5050@gmail.com>
2024-02-26 22:12:58 +0530

    Getting Started Boiss

--- app.vue
+++ app.vue
@@ -1,5 +1,51 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <a-layout has-sider>
+    
+    <a-layout :style="{ marginRight: '200px' }">
+      <a-layout-header :style="{ background: '#fff', padding: 0 }" />
+      <a-layout-content :style="{ margin: '24px 16px 0', overflow: 'initial' }">
+        <div :style="{ padding: '24px', background: '#fff', textAlign: 'center' }">
+            <CategoriesGrid />
+        </div>
+      </a-layout-content>
+      <a-layout-footer :style="{ textAlign: 'center' }">
+          FOOTER
+      </a-layout-footer>
+    </a-layout>
+
+    <a-layout-sider
+      :style="{ overflow: 'auto', height: '100vh', position: 'fixed', right: 0, top: 0, bottom: 0 }"
+    >
+      <p style="color: #fff;">BASKET</p>      
+    </a-layout-sider>
+
+  </a-layout>
 </template>
+<script lang="ts" setup>
+import { ref } from 'vue';
+import {
+  UserOutlined,
+  VideoCameraOutlined,
+  UploadOutlined,
+  BarChartOutlined,
+  CloudOutlined,
+  AppstoreOutlined,
+  TeamOutlined,
+  ShopOutlined,
+} from '@ant-design/icons-vue';
+const selectedKeys = ref<string[]>(['4']);
+</script>
+<style scoped>
+#components-layout-demo-fixed-sider .logo {
+  height: 32px;
+  background: rgba(255, 255, 255, 0.2);
+  margin: 16px;
+}
+.site-layout .site-layout-background {
+  background: #fff;
+}
+
+[data-theme='dark'] .site-layout .site-layout-background {
+  background: #141414;
+}
+</style>
---
+++ components/CategoriesGrid.vue
@@ -0,0 +1,72 @@
+<template>
+
+<div>
+
+<div class="CategoriesGrid">
+
+<div class="CategoryBlock">
+  <a-button class="ModalBTN" type="primary" @click="showModal">Category 1</a-button>
+</div>
+<div class="CategoryBlock">
+  <a-button class="ModalBTN" type="primary" @click="showModal">Category 2</a-button>
+</div>
+<div class="CategoryBlock">
+  <a-button class="ModalBTN" type="primary" @click="showModal">Category 3</a-button>
+</div>
+<div class="CategoryBlock">
+  <a-button class="ModalBTN" type="primary" @click="showModal">Category 4</a-button>
+</div>
+<div class="CategoryBlock">
+  <a-button class="ModalBTN" type="primary" @click="showModal">Category 5</a-button>
+</div>
+<div class="CategoryBlock">
+  <a-button class="ModalBTN" type="primary" @click="showModal">Category 6</a-button>
+</div>
+
+
+</div>
+
+<a-modal v-model:open="open" width="1000px" title="Category Name" @ok="handleOk">
+<p>Some contents...</p>
+<p>Some contents...</p>
+<p>Some contents...</p>
+</a-modal>
+
+</div>
+
+</template>
+
+<style scoped>
+
+
+
+.CategoriesGrid {
+    display: grid;
+    grid-template-columns: repeat(3,1fr);
+    grid-gap: 30px;
+    width: 300px;
+    margin: 0px auto;
+}
+.ModalBTN {
+  margin: 0px auto;
+  width: fit-content;
+  position: relative;
+    display: block;
+}
+
+
+</style>
+
+<script lang="ts" setup>
+import { ref } from 'vue';
+const open = ref<boolean>(false);
+
+const showModal = () => {
+  open.value = true;
+};
+
+const handleOk = (e: MouseEvent) => {
+  console.log(e);
+  open.value = false;
+};
+</script>
